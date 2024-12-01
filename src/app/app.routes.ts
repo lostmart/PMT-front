ceadc9e958cfa700cb56ca1969fe002a51831b36
lostmart <lostmart@hotmail.com>
2024-12-01 22:00:01 +0100

    new routes and routes provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LogInComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '', // Redirect to home or any other default route
+  },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   title = 'Project Management Tool';
+
+  handleClick() {
+    console.log('running the click ...');
+  }
 }
