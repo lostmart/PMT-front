26390a7b0736ffaae4f733eab0b508b40f267888
lostmart <lostmart@hotmail.com>
2025-01-24 18:53:16 +0100

    new data

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -12,32 +12,5 @@ import { RouterLink } from '@angular/router';
 })
 export class ProjectsComponent {
   projectsList: Project[] = [];
-  constructor() {
-    this.projectsList = [
-      {
-        id: 1,
-        name: 'Project 1',
-        description: 'Description 1',
-        startDate: '2021-01-01',
-        owner: 1,
-        tasks: [1, 2, 3],
-      },
-      {
-        id: 2,
-        name: 'Project 2',
-        description: 'Description 2',
-        startDate: '2021-02-01',
-        owner: 2,
-        tasks: [4, 5, 6],
-      },
-      {
-        id: 3,
-        name: 'Project 3',
-        description: 'Description 3',
-        startDate: '2021-03-01',
-        owner: 3,
-        tasks: [7, 8, 9],
-      },
-    ];
-  }
+  constructor() {}
 }
---
+++ src/app/service/project.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Project } from '../../types/Project';
+import { projectsListData } from './projects';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectService {
+  protected projectList: Project[] = projectsListData;
+
+  constructor() {}
+
+  getAllApartments(): Project[] {
+    return this.projectList;
+  }
+
+  getApartmentById(id: string): Project | undefined {
+    return this.projectList.find((project) => project?.id === parseInt(id, 10));
+  }
+}
