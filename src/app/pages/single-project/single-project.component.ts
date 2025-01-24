f4bf320448f3a251f4aebff5a75beb1019dac9df
lostmart <lostmart@hotmail.com>
2025-01-24 19:23:50 +0100

    new service

--- src/app/pages/single-project/single-project.component.ts
+++ src/app/pages/single-project/single-project.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProjectService } from '../../service/project.service';
+import { Project } from '../../../types/Project';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
   imports: [],
   templateUrl: './single-project.component.html',
-  styleUrl: './single-project.component.css'
+  styleUrl: './single-project.component.css',
 })
 export class SingleProjectComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: ProjectService = inject(ProjectService);
+  foundService: Project | null | undefined = null;
+  constructor() {
+    const id: string | null = this.route.snapshot.paramMap.get('id');
+    this.foundService = this.housingService.getProjectById(id ? id : '');
+  }
 }
