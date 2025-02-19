fa9d0d221efcf06ebc664a01d8937469e701f1f2
lostmart <lostmart@hotmail.com>
2025-02-19 19:36:21 +0100

    new

--- src/app/component/ui/button/button.component.ts
+++ src/app/component/ui/button/button.component.ts
@@ -14,8 +14,22 @@ export class ButtonComponent {
   @Input() text: string = 'text-zinc-100';
   @Input() onclick: Function | null = null;
   @Input() type: string | null = 'button';
+  @Input() size?: string | null = 'medium';
 
   handleBtnClick() {
     console.log('running the click ...');
+  }
+
+  getSize() {
+    switch (this.size) {
+      case 'small':
+        return 'h-8 w-32';
+      case 'medium':
+        return 'h-12 w-48';
+      case 'large':
+        return 'h-16 w-64';
+      default:
+        return 'h-12 w-48';
+    }
   }
 }
